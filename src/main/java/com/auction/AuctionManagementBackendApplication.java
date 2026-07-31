3759c9485dafeb5a44aa7532b82504eea79fb4ea
Amit Toradmal <amit.toradmal01@gmail.com>
2026-07-31 19:00:48 +0530

    Changed spring version

---
+++ src/main/java/com/auction/AuctionManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionManagementBackendApplication.class, args);
+	}
+
+}
